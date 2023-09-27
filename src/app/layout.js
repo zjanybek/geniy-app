db55070ddee68a81c0becb5f6aadd2ccf073cefc
Jony D <vip.abdylasov@mail.ru>
2023-09-28 02:39:46 +0600

    check

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,70 @@
+import { Montserrat } from 'next/font/google'
+import localFont from 'next/font/local'
+
 import TheFooter from '@/components/TheFooter/TheFooter'
 import TheHeader from '@/components/TheHeader/TheHeader'
 
 import '@/styles/globals.scss'
 
-// import { Inter } from 'next/font/google'
+const helveticaneuecyr = localFont({
+  src: [
+    {
+      path: '../fonts/helveticaneuecyr/helveticaneuecyr-roman.woff2',
+      style: 'normal',
+      weight: '400'
+    },
+    {
+      path: '../fonts/helveticaneuecyr/helveticaneuecyr-medium.woff2',
+      style: 'normal',
+      weight: '500'
+    },
+    {
+      path: '../fonts/helveticaneuecyr/helveticaneuecyr-italic.woff2',
+      style: 'italic',
+      weight: '500'
+    },
+    {
+      path: '../fonts/helveticaneuecyr/helveticaneuecyr-bold.woff2',
+      style: 'normal',
+      weight: '700'
+    }
+  ],
+  display: 'swap',
+  variable: '--font-family',
+  preload: true
+})
 
-// const inter = Inter({ subsets: ['latin'] })
+const baskervillecyrltstd = localFont({
+  src: [
+    {
+      path: '../fonts/baskervillecyrltstd/baskerville-cyr-ltstd-bold.woff2',
+      style: 'normal',
+      weight: '700'
+    }
+  ],
+  display: 'swap',
+  variable: '--second-family',
+  preload: true
+})
+
+const geologica = localFont({
+  src: [
+    {
+      path: '../fonts/geologica/geologica-regular.woff2',
+      style: 'normal',
+      weight: '400'
+    }
+  ],
+  display: 'swap',
+  variable: '--font5',
+  preload: true
+})
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font6',
+  display: 'swap'
+})
 
 export const metadata = {
   title: 'Гении',
@@ -14,7 +73,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
+    <html
+      lang='en'
+      className={`${baskervillecyrltstd.variable} ${helveticaneuecyr.variable} ${geologica.variable} ${montserrat.variable}`}
+    >
       <body>
         <div className='wrapper'>
           <TheHeader />
--- src/screens/MainPage/components/Questions.jsx
+++ src/screens/MainPage/components/Questions.jsx
@@ -20,53 +20,212 @@ export const Questions = () => {
             Часто задаваемые <span>вопросы</span>
           </h2>
           <div className='questions__items'>
-            {[1, 2].map((_, index) => {
-              return (
-                <div
-                  key={index}
-                  className={`questions__item ${
-                    isShow[index] ? 'questions__item-active' : ''
-                  }`}
-                >
-                  <button
-                    onClick={() => toggleCollapse(index)}
-                    className='questions__button'
+            <div
+              className={`questions__item ${
+                isShow[0] ? 'questions__item-active' : ''
+              }`}
+            >
+              <button
+                onClick={() => toggleCollapse(0)}
+                className='questions__button'
+              >
+                <h2 className='questions__button-text'>
+                  Как зарегистрироваться на платформе ‘Гений’?
+                </h2>
+                <span className='questions__button-arrow'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='23'
+                    height='14'
+                    viewBox='0 0 23 14'
+                    fill='none'
                   >
-                    <h2 className='questions__button-text'>
-                      Как зарегистрироваться на платформе ‘Гений’?
-                    </h2>
-                    <span className='questions__button-arrow'>
-                      <svg
-                        xmlns='http://www.w3.org/2000/svg'
-                        width='23'
-                        height='14'
-                        viewBox='0 0 23 14'
-                        fill='none'
-                      >
-                        <path
-                          d='M11.5 13.5706L0 2.42471L2.06042 0.427734L11.5 9.62309L20.9396 0.474176L23 2.47115L11.5 13.5706Z'
-                          fill='currentColor'
-                        />
-                      </svg>
-                    </span>
-                  </button>
-                  <div className={`questions__collapse`}>
-                    <div style={{ minHeight: 0 }}>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Quasi distinctio molestias deserunt quas temporibus, magni
-                      illum ipsa, minima sequi saepe ad voluptas amet nesciunt
-                      maiores, placeat cumque. Autem eaque temporibus non
-                      officia ullam unde explicabo fuga sapiente repudiandae
-                      quod, libero, aspernatur a optio aliquid quam omnis
-                      corporis facilis voluptates quisquam eos ab! Rem alias
-                      amet similique dolorum, atque maiores, eius enim eos
-                      ducimus reiciendis quis dicta obcaecati! Sequi mollitia ex
-                      fuga
-                    </div>
-                  </div>
+                    <path
+                      d='M11.5 13.5706L0 2.42471L2.06042 0.427734L11.5 9.62309L20.9396 0.474176L23 2.47115L11.5 13.5706Z'
+                      fill='currentColor'
+                    />
+                  </svg>
+                </span>
+              </button>
+              <div className={`questions__collapse`}>
+                <div style={{ minHeight: 0 }}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+                  distinctio molestias deserunt quas temporibus, magni illum
+                  ipsa, minima sequi saepe ad voluptas amet nesciunt maiores,
+                  placeat cumque. Autem eaque temporibus non officia ullam unde
+                  explicabo fuga sapiente repudiandae quod, libero, aspernatur a
+                  optio aliquid quam omnis corporis facilis voluptates quisquam
+                  eos ab! Rem alias amet similique dolorum, atque maiores, eius
+                  enim eos ducimus reiciendis quis dicta obcaecati! Sequi
+                  mollitia ex fuga
                 </div>
-              )
-            })}
+              </div>
+            </div>
+            <div
+              className={`questions__item ${
+                isShow[1] ? 'questions__item-active' : ''
+              }`}
+            >
+              <button
+                onClick={() => toggleCollapse(1)}
+                className='questions__button'
+              >
+                <h2 className='questions__button-text'>
+                  Как создать и настроить профиль исполнителя или заказчика?
+                </h2>
+                <span className='questions__button-arrow'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='23'
+                    height='14'
+                    viewBox='0 0 23 14'
+                    fill='none'
+                  >
+                    <path
+                      d='M11.5 13.5706L0 2.42471L2.06042 0.427734L11.5 9.62309L20.9396 0.474176L23 2.47115L11.5 13.5706Z'
+                      fill='currentColor'
+                    />
+                  </svg>
+                </span>
+              </button>
+              <div className={`questions__collapse`}>
+                <div style={{ minHeight: 0 }}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+                  distinctio molestias deserunt quas temporibus, magni illum
+                  ipsa, minima sequi saepe ad voluptas amet nesciunt maiores,
+                  placeat cumque. Autem eaque temporibus non officia ullam unde
+                  explicabo fuga sapiente repudiandae quod, libero, aspernatur a
+                  optio aliquid quam omnis corporis facilis voluptates quisquam
+                  eos ab! Rem alias amet similique dolorum, atque maiores, eius
+                  enim eos ducimus reiciendis quis dicta obcaecati! Sequi
+                  mollitia ex fuga
+                </div>
+              </div>
+            </div>
+            <div
+              className={`questions__item ${
+                isShow[2] ? 'questions__item-active' : ''
+              }`}
+            >
+              <button
+                onClick={() => toggleCollapse(2)}
+                className='questions__button'
+              >
+                <h2 className='questions__button-text'>
+                  Могу ли я иметь одновременно несколько учетных записей на
+                  одной бирже?
+                </h2>
+                <span className='questions__button-arrow'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='23'
+                    height='14'
+                    viewBox='0 0 23 14'
+                    fill='none'
+                  >
+                    <path
+                      d='M11.5 13.5706L0 2.42471L2.06042 0.427734L11.5 9.62309L20.9396 0.474176L23 2.47115L11.5 13.5706Z'
+                      fill='currentColor'
+                    />
+                  </svg>
+                </span>
+              </button>
+              <div className={`questions__collapse`}>
+                <div style={{ minHeight: 0 }}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+                  distinctio molestias deserunt quas temporibus, magni illum
+                  ipsa, minima sequi saepe ad voluptas amet nesciunt maiores,
+                  placeat cumque. Autem eaque temporibus non officia ullam unde
+                  explicabo fuga sapiente repudiandae quod, libero, aspernatur a
+                  optio aliquid quam omnis corporis facilis voluptates quisquam
+                  eos ab! Rem alias amet similique dolorum, atque maiores, eius
+                  enim eos ducimus reiciendis quis dicta obcaecati! Sequi
+                  mollitia ex fuga
+                </div>
+              </div>
+            </div>
+            <div
+              className={`questions__item ${
+                isShow[3] ? 'questions__item-active' : ''
+              }`}
+            >
+              <button
+                onClick={() => toggleCollapse(3)}
+                className='questions__button'
+              >
+                <h2 className='questions__button-text'>
+                  Как настроить параметры проекта и указать бюджет?
+                </h2>
+                <span className='questions__button-arrow'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='23'
+                    height='14'
+                    viewBox='0 0 23 14'
+                    fill='none'
+                  >
+                    <path
+                      d='M11.5 13.5706L0 2.42471L2.06042 0.427734L11.5 9.62309L20.9396 0.474176L23 2.47115L11.5 13.5706Z'
+                      fill='currentColor'
+                    />
+                  </svg>
+                </span>
+              </button>
+              <div className={`questions__collapse`}>
+                <div style={{ minHeight: 0 }}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+                  distinctio molestias deserunt quas temporibus, magni illum
+                  ipsa, minima sequi saepe ad voluptas amet nesciunt maiores,
+                  placeat cumque. Autem eaque temporibus non officia ullam unde
+                  explicabo fuga sapiente repudiandae quod, libero, aspernatur a
+                  optio aliquid quam omnis corporis facilis voluptates quisquam
+                  eos ab! Rem alias amet similique dolorum, atque maiores, eius
+                  enim eos ducimus reiciendis quis dicta obcaecati! Sequi
+                  mollitia ex fuga
+                </div>
+              </div>
+            </div>
+            <div
+              className={`questions__item ${
+                isShow[4] ? 'questions__item-active' : ''
+              }`}
+            >
+              <button
+                onClick={() => toggleCollapse(4)}
+                className='questions__button'
+              >
+                <h2 className='questions__button-text'>
+                  Как найти подходящего исполнителя для моего проекта?
+                </h2>
+                <span className='questions__button-arrow'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='23'
+                    height='14'
+                    viewBox='0 0 23 14'
+                    fill='none'
+                  >
+                    <path
+                      d='M11.5 13.5706L0 2.42471L2.06042 0.427734L11.5 9.62309L20.9396 0.474176L23 2.47115L11.5 13.5706Z'
+                      fill='currentColor'
+                    />
+                  </svg>
+                </span>
+              </button>
+              <div className={`questions__collapse`}>
+                <div style={{ minHeight: 0 }}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+                  distinctio molestias deserunt quas temporibus, magni illum
+                  ipsa, minima sequi saepe ad voluptas amet nesciunt maiores,
+                  placeat cumque. Autem eaque temporibus non officia ullam unde
+                  explicabo fuga sapiente repudiandae quod, libero, aspernatur a
+                  optio aliquid quam omnis corporis facilis voluptates quisquam
+                  eos ab! Rem alias amet similique dolorum, atque maiores, eius
+                  enim eos ducimus reiciendis quis dicta obcaecati! Sequi
+                  mollitia ex fuga
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
