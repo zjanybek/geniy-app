0d6a6f0f82162e85222910d1d464252c1273322d
Jony D <vip.abdylasov@mail.ru>
2023-10-10 15:22:33 +0600

    load

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import { Geologica, Montserrat } from 'next/font/google'
+import { Geologica, Montserrat, Roboto } from 'next/font/google'
 import localFont from 'next/font/local'
 
 import Providers from '@/components/Providers'
@@ -10,80 +10,70 @@ import '@/styles/globals.scss'
 const helveticaneuecyr = localFont({
   src: [
     {
-      path: '../fonts/neue-montreal/NeueMontreal-Regular.woff2',
+      path: '../fonts/new-test/HelveticaNeueCyr-Medium.woff2',
       style: 'normal',
       weight: '400'
     },
     {
-      path: '../fonts/neue-montreal/NeueMontreal-Medium.woff2',
+      path: '../fonts/new-test/HelveticaNeueCyr-Medium.woff2',
       style: 'normal',
       weight: '500'
     },
     {
-      path: '../fonts/neue-montreal/NeueMontreal-Bold.woff2',
+      path: '../fonts/helveticaneuecyr-2/HelveticaNeueCyr-Black.woff2',
+      style: 'normal',
+      weight: '900'
+    },
+    {
+      path: '../fonts/helveticaneuecyr-2/HelveticaNeueCyr-BlackItalic.woff2',
+      style: 'italic',
+      weight: '900'
+    },
+    {
+      path: '../fonts/helveticaneuecyr-2/HelveticaNeueCyr-Bold.woff2',
       style: 'normal',
       weight: '700'
     },
     {
-      path: '../fonts/neue-montreal/NeueMontreal-Italic.woff2',
+      path: '../fonts/helveticaneuecyr-2/HelveticaNeueCyr-BoldItalic.woff2',
+      style: 'italic',
+      weight: '700'
+    },
+    {
+      path: '../fonts/helveticaneuecyr-2/HelveticaNeueCyr-Italic.woff2',
       style: 'italic',
       weight: '400'
+    },
+    {
+      path: '../fonts/helveticaneuecyr-2/HelveticaNeueCyr-Light.woff2',
+      style: 'normal',
+      weight: '300'
+    },
+    {
+      path: '../fonts/helveticaneuecyr-2/HelveticaNeueCyr-LightItalic.woff2',
+      style: 'italic',
+      weight: '300'
+    },
+    {
+      path: '../fonts/helveticaneuecyr-2/HelveticaNeueCyr-Thin.woff2',
+      style: 'normal',
+      weight: '100'
+    },
+    {
+      path: '../fonts/helveticaneuecyr-2/HelveticaNeueCyr-ThinItalic.woff2',
+      style: 'italic',
+      weight: '100'
+    },
+    {
+      path: '../fonts/helveticaneuecyr-2/HelveticaNeueCyr-UltraLight.woff2',
+      style: 'normal',
+      weight: '200'
+    },
+    {
+      path: '../fonts/helveticaneuecyr-2/HelveticaNeueCyr-UltraLightItalic.woff2',
+      style: 'italic',
+      weight: '200'
     }
-    // {
-    //   path: '../fonts/helveticaneuecyr-2/HelveticaNeueCyr-Black.woff2',
-    //   style: 'normal',
-    //   weight: '900'
-    // },
-    // {
-    //   path: '../fonts/helveticaneuecyr-2/HelveticaNeueCyr-BlackItalic.woff2',
-    //   style: 'italic',
-    //   weight: '900'
-    // },
-    // {
-    //   path: '../fonts/helveticaneuecyr-2/HelveticaNeueCyr-Bold.woff2',
-    //   style: 'normal',
-    //   weight: '700'
-    // },
-    // {
-    //   path: '../fonts/helveticaneuecyr-2/HelveticaNeueCyr-BoldItalic.woff2',
-    //   style: 'italic',
-    //   weight: '700'
-    // },
-    // {
-    //   path: '../fonts/helveticaneuecyr-2/HelveticaNeueCyr-Italic.woff2',
-    //   style: 'italic',
-    //   weight: '400'
-    // },
-    // {
-    //   path: '../fonts/helveticaneuecyr-2/HelveticaNeueCyr-Light.woff2',
-    //   style: 'normal',
-    //   weight: '300'
-    // },
-    // {
-    //   path: '../fonts/helveticaneuecyr-2/HelveticaNeueCyr-LightItalic.woff2',
-    //   style: 'italic',
-    //   weight: '300'
-    // },
-    // {
-    //   path: '../fonts/helveticaneuecyr-2/HelveticaNeueCyr-Thin.woff2',
-    //   style: 'normal',
-    //   weight: '100'
-    // },
-    // {
-    //   path: '../fonts/helveticaneuecyr-2/HelveticaNeueCyr-ThinItalic.woff2',
-    //   style: 'italic',
-    //   weight: '100'
-    // },
-    // {
-    //   path: '../fonts/helveticaneuecyr-2/HelveticaNeueCyr-UltraLight.woff2',
-    //   style: 'normal',
-    //   weight: '200'
-    // },
-    // {
-    //   path: '../fonts/helveticaneuecyr-2/HelveticaNeueCyr-UltraLightItalic.woff2',
-    //   style: 'italic',
-    //   weight: '200'
-    // }
   ],
   // fallback: [
   //   'Neue Montreal',
@@ -115,6 +105,14 @@ const baskervillecyrltstd = localFont({
   preload: true
 })
 
+const roboto = Roboto({
+  subsets: ['cyrillic'],
+  weight: ['100', '300', '400', '500', '700', '900'],
+  display: 'swap',
+  variable: '--font-family',
+  preload: true
+})
+
 export const metadata = {
   title: 'Next.js 13.5',
   description: 'React server components (SSR, SSG, ISR, CSR)'
@@ -123,9 +121,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body
-        className={`${helveticaneuecyr.variable} ${baskervillecyrltstd.variable}`}
-      >
+      <body className={`${roboto.variable} ${baskervillecyrltstd.variable}`}>
         <Providers>
           <div className='wrapper'>
             <TheHeader />
